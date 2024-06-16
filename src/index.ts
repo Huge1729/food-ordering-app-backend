e4bc35878f125cc24bcafec2add7604419cbe1d8
Huge1729 <vishthehuge22@gmail.com>
2024-06-16 21:46:19 +0530

    updated till myrestaurant-api

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@ import express, {Request , Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute";
+import myrestaurantRoute from "./routes/MyRestaurantRoute"
+import { v2 as cloudinary} from "cloudinary";
 
 // Check the connection of database
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(()=> console.log("Connected to DataBase"));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 
 const app = express();   // create express server 
@@ -20,10 +28,11 @@ app.get("/health" , async (req: Request , res:Response)=>{
 
 // /api/my/user
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myrestaurantRoute);
 
 
 
 app.listen(7000, () => {
-    console.log("Server started on port: 3000");
+    console.log("Server started on port: 7000");
 });
 
