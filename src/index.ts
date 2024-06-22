af063bff09187bc46316fd7337f766a37be12841
Huge1729 <vishthehuge22@gmail.com>
2024-06-22 22:01:26 +0530

    added stripe/payment

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,40 @@
-import express, {Request , Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
-import myrestaurantRoute from "./routes/MyRestaurantRoute"
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
-import { v2 as cloudinary} from "cloudinary";
+import orderRoute from "./routes/OrderRoute";
 
-// Check the connection of database
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
-.then(()=> console.log("Connected to DataBase"));
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+const app = express();
 
-const app = express();   // create express server 
-app.use(express.json()); // like middleware that convert automatically the body into json 
 app.use(cors());
 
-// define end point to check Deployment
-app.get("/health" , async (req: Request , res:Response)=>{
-    res.send({message : "Health OK!!"});
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
 });
 
-// /api/my/user
 app.use("/api/my/user", myUserRoute);
-app.use("/api/my/restaurant", myrestaurantRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
-
+app.use("/api/order", orderRoute);
 
 app.listen(7000, () => {
-    console.log("Server started on port: 7000");
+  console.log("server started on localhost:7000");
 });
-
